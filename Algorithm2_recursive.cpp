0a9c6eb62a16c47f57b6c13077e1ab02f354008a
Joe0mran <yousif.edu.omran@gmail.com>
2026-05-13 23:29:53 +0300

    Replace sort with mergeSort for array sorting

--- Algorithm2_recursive.cpp
+++ Algorithm2_recursive.cpp
@@ -1,5 +1,40 @@
+
 #include<bits/stdc++.h>
 using namespace std;
+
+void mergeArr(int arr[], int l, int mid, int r){
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+    int left[n1], right[n2];
+    
+    for(int i = 0; i < n1; i++) left[i] = arr[l + i];
+    for(int i = 0; i < n2; i++) right[i] = arr[mid + 1 + i];
+    
+    int i = 0, j = 0, k = l;
+    while(i < n1 && j < n2){
+        if(left[i] <= right[j]){
+            arr[k++] = left[i++];
+        }else{
+            arr[k++] = right[j++];
+        }
+    }
+    while(i < n1){
+        arr[k++] = left[i++];
+    }
+    while(j < n2){
+        arr[k++] = right[j++];
+    }
+}
+
+void mergeSort(int arr[], int l, int r){
+    if(l >= r) return;
+    
+    int mid = (l + r) / 2;
+    mergeSort(arr, l, mid);
+    mergeSort(arr, mid + 1, r);
+    mergeArr(arr, l, mid, r);
+}
+
 int rec(int i, int ans, int arr[], int size){
     if(i == size) return ans;
     if(arr[i] == ans){
@@ -7,12 +42,13 @@ int rec(int i, int ans, int arr[], int size){
     }
     return rec(i + 1, ans, arr, size);
 }
+
 int main(){
     int n;
     cin >> n;
     int arr[n];
     for(int i = 0; i < n; i++) cin >> arr[i];
-    sort(arr, arr + n);
+    mergeSort(arr, 0, n-1);
     cout << rec(0, 1, arr, n) << endl;    
 }
 /*
